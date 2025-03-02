e3d48933e2dc32406a0119c037b2c51bdbe00e08
canifty <can-dindar@outlook.com>
2025-03-02 15:07:10 +0100

    First commit

---
+++ Hokkabaz/HokkabazApp.swift
@@ -0,0 +1,17 @@
+//
+//  HokkabazApp.swift
+//  Hokkabaz
+//
+//  Created by Can Dindar on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct HokkabazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
