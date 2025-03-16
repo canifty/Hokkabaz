5252a1878a5fbe526ef0a9efebf723b224758b42
canifty <can-dindar@outlook.com>
2025-03-16 21:13:09 +0100

    permission to save photos

--- Hokkabaz/ContentView.swift
+++ Hokkabaz/ContentView.swift
@@ -136,10 +136,7 @@ struct ContentView: View {
             }
         }
         .preferredColorScheme(preferredColorScheme)
-//        .sheet(isPresented: $viewModel.showTutorial) {
-//            TutorialView(theme: viewModel.appTheme, colorScheme: colorScheme)
-//                .preferredColorScheme(preferredColorScheme)
-//        }
+
         .animation(.interactiveSpring(duration: 0.5), value: viewModel.showSettings)
         .animation(.interactiveSpring(duration: 0.5), value: viewModel.showExportMenu)
         .animation(.easeInOut(duration: 0.3), value: viewModel.appTheme)
